1e039612316054b72296be2ba027bba7b9ed85ec
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 12:05:09 +0530

    Update Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -1,11 +1,10 @@
 public class Pattern3 {
-    public static void main(String[] args) {
-        int n = 5;
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= 5; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-    }
+ public static void main(String[] args) {
+ for (int i = 0; i < 5; i++) {
+for (int j = 1; j <= 5; j++) {
+   System.out.print(j + " ");
+ }
+   System.out.println();
+  }
+  }
 }
