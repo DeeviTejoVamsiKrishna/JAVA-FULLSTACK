713e92ed92a98a0572ee3735843a9b390162346b
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 12:07:15 +0530

    Update Pattern5.java

--- Pattern5.java
+++ Pattern5.java
@@ -1,14 +1,14 @@
 public class Pattern5 {
-    public static void main(String[] args) {
-        int n = 5, num = 1;
-        for (int i = 0; i < n; i++) {
-            int val = num;
-            for (int j = 0; j < n; j++) {
-                System.out.print(val + " ");
-                val = (val % 9) + 1;
-            }
-            num = (num % 9) + 1;
-            System.out.println();
-        }
+ public static void main(String[] args) {
+  int n = 5, num = 1;
+ for (int i = 0; i < n; i++) {
+ int val = num;
+ for (int j = 0; j < n; j++) {
+  System.out.print(val + " ");
+   val = (val % 9) + 1;
+    }
+  num = (num % 9) + 1;
+    System.out.println();
+      }
     }
 }
