90f85d20fab4acee3fa4b1751b9d6919184a7817
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 11:52:35 +0530

    Create Pattern10 .java

---
+++ Pattern10 .java
@@ -0,0 +1,11 @@
+public class Pattern10 {
+    public static void main(String[] args) {
+        String word = "INDIA";
+        for (int i = 1; i <= word.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(word.charAt(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
