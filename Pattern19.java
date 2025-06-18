ab6b334f53ec4ea69dafc66937646cfd1c419785
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 12:24:38 +0530

    Update Pattern19.java

--- Pattern19.java
+++ Pattern19.java
@@ -1,9 +1,9 @@
 public class Pattern19 {
-    public static void main(String[] args) {
-        int n = 6;
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < i; j++) System.out.print("* ");
-            System.out.println(i);
-        }
-    }
+public static void main(String[] args) {
+int n = 6;
+for (int i = 0; i <= n; i++) {
+for (int j = 0; j < i; j++) System.out.print("* ");
+System.out.println(i);
+}
+}
 }
