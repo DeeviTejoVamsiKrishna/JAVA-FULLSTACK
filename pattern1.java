08c5a42d99acb329d2b9a25e2732bfe06f575a67
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 11:47:18 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,11 +1,8 @@
-public class Pattern1 {
-    public static void main(String[] args) {
-        int n = 6;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
+public class Pattern1 {public static void main(String[] args) {
+for (int i = 1; i <= 6; i++) {
+for (int j = 1; j <= 6; j++) {
+System.out.print("*"); }
+System.out.println();
+}
+}
 }
