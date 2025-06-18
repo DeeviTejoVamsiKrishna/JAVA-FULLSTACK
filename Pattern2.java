483261584713682a723bf6ed2377aa53cfb381eb
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 12:03:31 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,11 +1,10 @@
 public class Pattern2 {
-    public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 5; j++) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-        }
-    }
+public static void main(String[] args) {
+ for (int i = 1; i <= 5; i++) {
+ for (int j = 1; j <= 5; j++) {
+ System.out.print(i + " ");
+}
+  System.out.println();
+ }
+ }
 }
