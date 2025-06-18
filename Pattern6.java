ad96bc34c4678a2672810e3a5e348e9f75e13e4b
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 12:08:47 +0530

    Update Pattern6.java

--- Pattern6.java
+++ Pattern6.java
@@ -1,14 +1,14 @@
 public class Pattern6 {
-    public static void main(String[] args) {
-        int n = 5, num = 1;
-        for (int i = 0; i < n; i++) {
-            int val = num;
-            for (int j = 0; j < n; j++) {
-                System.out.print(val + " ");
-                val += 2;
-            }
-            num += 20;
-            System.out.println();
+ public static void main(String[] args) {
+int n = 5, num = 1;
+for (int i = 0; i < n; i++) {
+ int val = num;
+ for (int j = 0; j < n; j++) {
+ System.out.print(val + " ");
+ val += 2;
+     }
+   num += 20;
+   System.out.println();
         }
     }
 }
