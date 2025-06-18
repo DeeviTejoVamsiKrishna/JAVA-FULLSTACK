a9cb0aeb0d6ed046c12ae7f47a5e63144096c530
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 19:17:39 +0530

    Create Pattern12.java

---
+++ Pattern12.java
@@ -0,0 +1,33 @@
+public class Pattern12
+{
+public static void main(String[] args) 
+{
+int n = 4;
+for(int i=1;i<=n;i++) 
+{
+int spaces=13-2*i;
+for(int s=0;s<spaces;s++)
+{
+System.out.print(" ");
+}
+for(int j=1;j<=2*i-1;j++) 
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+for(int i=n-1;i>=1;i--) 
+{
+int spaces=13-2*i;
+for(int s=0;s<spaces;s++) 
+{
+System.out.print(" ");
+}
+for(int j=1;j<=2*i-1;j++) 
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}*/
