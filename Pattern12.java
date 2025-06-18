43321d747d472cdd2d1e56d343eba5993d358900
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 12:16:00 +0530

    Update Pattern12.java

--- Pattern12.java
+++ Pattern12.java
@@ -1,18 +1,16 @@
 public class Pattern12 {
-    public static void main(String[] args) {
-        int n = 7;
-        int mid = n / 2;
-
-        for (int i = 0; i <= mid; i++) {
-            for (int s = 0; s < mid - i; s++) System.out.print("  ");
-            for (int j = 0; j < 2 * i + 1; j++) System.out.print("* ");
-            System.out.println();
-        }
-
-        for (int i = mid - 1; i >= 0; i--) {
-            for (int s = 0; s < mid - i; s++) System.out.print("  ");
-            for (int j = 0; j < 2 * i + 1; j++) System.out.print("* ");
-            System.out.println();
-        }
-    }
+public static void main(String[] args) {
+int n = 7;
+int mid = n / 2;
+for (int i = 0; i <= mid; i++) {
+  for (int s = 0; s < mid - i; s++) System.out.print("  ");
+     for (int j = 0; j < 2 * i + 1; j++) System.out.print("* ");
+   System.out.println();
+  }
+ for (int i = mid - 1; i >= 0; i--) {
+   for (int s = 0; s < mid - i; s++) System.out.print("  ");
+      for (int j = 0; j < 2 * i + 1; j++) System.out.print("* ");
+   System.out.println();
+  }
+ }
 }
