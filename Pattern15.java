59961fa5692b9a63075ff7ab71af5ccb3e9cb6d5
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 13:42:34 +0530

    Delete Pattern15.java

--- Pattern15.java
+++
@@ -1,8 +0,0 @@
-public class Pattern15 {
-public static void main(String[] args) {
-int n = 3;
-for (int i = 0; i < n; i++) System.out.println("   *");
-System.out.println("*****");
-for (int i = 0; i < n; i++) System.out.println("   *");
- }
-}
