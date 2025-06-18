9271ba811cf066287531f12b78d67d41f71b8ed8
DeeviTejoVamsiKrishna <deevivamsi@gmail.com>
2025-06-18 12:14:30 +0530

    Update Pattern11.java

--- Pattern11.java
+++ Pattern11.java
@@ -1,10 +1,9 @@
 public class Pattern11 {
-    public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int s = 1; s <= n - i; s++) System.out.print("  ");
-            for (int j = 1; j <= 2 * i - 1; j++) System.out.print("* ");
-            System.out.println();
-        }
-    }
+public static void main(String[] args) {
+ for (int i = 1; i <= 5; i++) {
+ for (int s = 1; s <= 5 - i; s++) System.out.print("  ");
+ for (int j = 1; j <= 2 * i - 1; j++) System.out.print("* ");
+ System.out.println();
+  }
+ }
 }
